bc5ead3ed0a0d4471d5a4a9398b6c0b5c47509e6
4mh23cs014-cpu <4mh23cs014@gmail.com>
2026-02-11 12:50:03 +0530

    dfg

--- my-project/src/components/Header.jsx
+++ my-project/src/components/Header.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
-import { Link  } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <div>
-        <h1>header  </h1>
         <nav style={{ padding: '1rem', backgroundColor: '#333', marginBottom: '1rem' }}>
         <Link to="/" style={{ color: 'white', marginRight: '1rem' }}>Home</Link>
         <Link to="/about" style={{ color: 'white', marginRight: '1rem' }}>About</Link>
-        <Link to="/contact" style={{ color: 'white' }}>Contact</Link>
-        <Link to="/signup" style={{ color: 'white' }}>Signup</Link>
-        <Link to="/login" style={{ color: 'white' }}>Login</Link> 
+        <Link to="/contact" style={{ color: 'white', marginRight: '1rem' }}>Contact</Link>
+        <Link to="/login" style={{ color: 'white', marginRight: '1rem' }}>Login</Link>
+        <Link to="/signup" style={{ color: 'white', marginRight: '1rem' }}>Signup</Link>
       </nav>
     </div>
   )
 }
 
-export default  Header
+export default Header
 
-//login and signup need to be add,login need to be added
+// login sign up needts to be added , logo needs to be added
